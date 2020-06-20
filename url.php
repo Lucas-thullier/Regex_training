be0ef98e13e23c22e4ec08eb13fc7f9ba2366e82
Lucas Thullier <lucas.thullier@outlook.fr>
2020-06-20 15:51:40 +0200

    improvement of url detection and reject bad url

--- url.php
+++ url.php
@@ -1,5 +1,5 @@
 <?php
 
 $texte = "https://www.openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql/917386-les-expressions-regulieres-partie-2-2";
-echo preg_replace("#^((https?://)?(w{3})?)?\.[\d\w./-]+\.[a-z]{2,4}[\d\w./_-]*$#", "<a href=$0>$0</a>", $texte);
+echo preg_replace("#^((https?:\/\/)?(w{3}\.)?)([^w]{3}[\w\/-]+)\.([a-z]{2,4})(\/([\w\/-]*))*#", "<a href=$0>$0</a>", $texte);
 
